601d27fd83e7043258d897aff4138a287d325f68
Vlad Denikeev <vlad-kempo@yandex.ru>
2021-06-15 20:11:21 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrow:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:5,
+        slidesToScroll:4
+
+    });
+});
