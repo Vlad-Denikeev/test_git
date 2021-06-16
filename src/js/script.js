1f11bf6ec1f4746c7e6a79c2a1f135e3093b5744
Vlad Denikeev <vlad-kempo@yandex.ru>
2021-06-16 22:00:58 +0300

    added box5

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
         arrow:true,
         dots:true,
         adaptiveHeight:true,
-        slidesToShow:1,
+        slidesToShow:3,
         slidesToScroll:1,
         speed:1000,
         centerMode:true,
         variableWight:true
-
     });
 });
