736b367e99822f933978b66c33f23b40b5588c91
Vlad Denikeev <vlad-kempo@yandex.ru>
2021-06-24 12:57:57 +0500

    good_mobil

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
     $('.slider').slick({
         arrow:true,
         dots:true,
-        adaptiveHeight:true,
+        adaptiveHeight:false,
         slidesToShow:3,
         slidesToScroll:1,
         speed:1000,
         centerMode:true,
-        variableWight:true
+        variableWight:true,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:2
+                }
+            },{
+                breakpoint: 480,
+                settings: {
+                    slidesToShow:1,
+                    slidesToScroll:2
+                }
+            }
+        ]
     });
 });
